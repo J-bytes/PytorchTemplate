d1dbc731a19f24a5ef273e1c6203d13294bbe327
J-bytes <60334318+J-bytes@users.noreply.github.com>
2022-11-24 10:24:51 -0500

    pushed new development

---
+++ models/GAN.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2022-11-22$
+
+@author: Jonathan Beaulieu-Emond
+"""
+
+
+def main():
+    return "hello World"
+
+
+if __name__ == "__main__":
+    main()
